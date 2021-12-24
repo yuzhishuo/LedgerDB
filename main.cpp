0674d4d039a57eee9d1720e74c58be11fb24be15
yimin <yimin.muduo@outlook.com>
2021-12-24 11:17:15 +0800

    add rbac example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
-#include <add.h>
+#include <casbin/casbin.h>
+#include <memory>
 
-int main(int, char**) {
-    std::cout << add(1,1) << std::endl;
+int main(int, char **)
+{
+
+    // auto e = casbin::Enforcer("rbac/rbac_model.conf", "rbac/rbac_policy.csv");
+
+    auto m = std::make_shared<casbin::Model>("rbac/rbac_model.conf");
+
+    auto a = std::make_shared<casbin::FileAdapter>("rbac/rbac_policy.csv");
+
+    auto e = casbin::Enforcer((m), std::dynamic_pointer_cast<casbin::Adapter>(a));
+
+    auto sub = "alice";
+    auto obj = "data1";
+    auto act = "read";
+
+    auto ok = e.Enforce({sub, obj, act});
+
+    if (ok)
+    {
+        std::cout << "auth sucessor" << std::endl;
+    }
+
+    auto roles = e.GetRolesForUser(sub);
+
+    for (auto role : roles)
+    {
+        std::cout << "sub: " << sub << ",role is " << role << std::endl;
+    }
+
+    return 0;
 }

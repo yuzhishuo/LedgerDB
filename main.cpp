6110f42661ea300e75dc9afd29f9916a17eaa6e9
yimin <yimin.muduo@outlook.com>
2021-12-29 18:52:54 +0800

    init

--- main.cpp
+++ main.cpp
@@ -4,32 +4,57 @@
 
 int main(int, char **)
 {
-
-    // auto e = casbin::Enforcer("rbac/rbac_model.conf", "rbac/rbac_policy.csv");
-
-    auto m = std::make_shared<casbin::Model>("rbac/rbac_model.conf");
-
-    auto a = std::make_shared<casbin::FileAdapter>("rbac/rbac_policy.csv");
-
-    auto e = casbin::Enforcer((m), std::dynamic_pointer_cast<casbin::Adapter>(a));
-
-    auto sub = "alice";
-    auto obj = "data1";
-    auto act = "read";
-
-    auto ok = e.Enforce({sub, obj, act});
-
-    if (ok)
     {
-        std::cout << "auth sucessor" << std::endl;
+        auto m = std::make_shared<casbin::Model>("./rbac/user/rbac_model.conf");
+
+        auto a = std::make_shared<casbin::FileAdapter>("./rbac/user/rbac_policy.csv");
+
+        auto e = casbin::Enforcer(m, std::dynamic_pointer_cast<casbin::Adapter>(a));
+
+        auto sub = "admin";
+        auto obj = "CREATE_USER";
+
+        auto ok = e.Enforce({sub, obj});
+
+        if (ok)
+        {
+            std::cout << "auth sucessor" << std::endl;
+        }
+
+        auto roles = e.GetRolesForUser(sub);
+
+        for (auto role : roles)
+        {
+            std::cout << "sub: " << sub << ",role is " << role << std::endl;
+        }
     }
 
-    auto roles = e.GetRolesForUser(sub);
-
-    for (auto role : roles)
     {
-        std::cout << "sub: " << sub << ",role is " << role << std::endl;
-    }
 
+        auto m = std::make_shared<casbin::Model>("./rbac/ledger/rbac_model.conf");
+
+        auto a = std::make_shared<casbin::FileAdapter>("./rbac/ledger/rbac_policy.csv");
+
+        auto e = casbin::Enforcer(m, std::dynamic_pointer_cast<casbin::Adapter>(a));
+
+        auto sub = "admin";
+        auto obj = "Read";
+        auto act = "luluyuzhi";
+
+        auto ok = e.Enforce({sub, obj, act});
+
+        if (ok)
+        {
+            std::cout << "auth sucessor" << std::endl;
+        }
+
+        auto roles = e.GetRolesForUser(sub);
+
+        for (auto role : roles)
+        {
+            std::cout << "sub: " << sub << ",role is " << role << std::endl;
+        }
+
+    }
     return 0;
 }

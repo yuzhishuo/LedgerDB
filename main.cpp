b041f0de5ceb2061cdd223a6a883ba0f6d37bddc
yimin <aoumeior@outlook.com>
2022-01-29 23:19:47 +0800

    add raft

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-25 21:35:46
+ * @LastEditTime: 2022-01-29 22:24:59
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /example-authority-cpp/main.cpp
+ */
 #define SPDLOG_TRACE_ON
 #define SPDLOG_DEBUG_ON
 
@@ -15,6 +23,12 @@
 #include "IUnique.h"
 #include "Users.h"
 #include "Ledgers.h"
+extern "C"
+{
+#include <raft.h>
+}
+
+#include <muduo/net/EventLoop.h>
 // tmp
 namespace spd = spdlog;
 
@@ -42,6 +56,10 @@ int main(int argc, char **argv)
 
         auto en = l->engine();
     }
+
+    auto e = muduo::net::EventLoop();
+
+    raft_server_t *raft = raft_new();
 
     // auto console = spd::stdout_color_mt("console");
     // spd::get("console")->info("Hello, world!");

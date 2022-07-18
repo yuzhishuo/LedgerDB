6200e9ebaf43e18d5c9e66796a5a161d56f05940
muduo <aoumeior@outlook.com>
2022-07-18 10:26:43 +0000

    1

---
+++ include/meta/LedgersImpl.h
@@ -0,0 +1,28 @@
+/*
+ * @Author: Leo
+ * @Date: 2022-07-17 14:09:42
+ * @LastEditors: Leo
+ * @LastEditTime: 2022-07-18 10:23:45
+ */
+#pragma once
+
+class LedgersImpl
+{
+public:
+    LedgersImpl();
+    ~LedgersImpl();
+
+private:
+
+    /*
+        create ledger with name and owner
+        create user 
+    */
+    // createLedger  
+    
+    /*
+        delete user
+        delete ledger
+    */
+    // deleteLedger
+};
